3cc0055f46a132edfbfe5284ced7b7cdbb736448
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:22:56 +0800

    状态模式

--- src/StatePattern/test.java
+++ src/StatePattern/test.java
@@ -5,8 +5,8 @@ public class test {
     public static void main(String[] args) {
         Context context=new Context(20);
         State state=new MorningState();
-        state.doSomething(context);
+        state.doSomething(context); //不吃饭！
         context.setTime(12);
-        state.doSomething(context);
+        state.doSomething(context); //吃午饭！
     }
 }
