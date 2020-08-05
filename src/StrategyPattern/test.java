5f000032070f6a423b6d7d3f2e1281f880f4e899
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:23:58 +0800

    策略模式

--- src/StrategyPattern/test.java
+++ src/StrategyPattern/test.java
@@ -3,12 +3,12 @@ package StrategyPattern;
 public class test {
     public static void main(String[] args) {
         Context context = new Context(new OperationAdd());
-        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5)); //10 + 5 = 15
 
         context = new Context(new OperationSubtract());
-        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5)); //10 - 5 = 5
 
         context = new Context(new OperationMultiply());
-        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5)); //10 * 5 = 50
     }
 }
