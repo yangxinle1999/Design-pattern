1f7f48c34a621b85211fdc34d599a641c90b8419
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:15:14 +0800

    桥接模式

--- src/BridgePattern/test.java
+++ src/BridgePattern/test.java
@@ -6,8 +6,8 @@ public class test {
         Engine2000cc engine2000cc=new Engine2000cc();
         Engine2200cc engine2200cc=new Engine2200cc();
         Car bus=new Bus(engine2000cc);
-        bus.InstallEngine();
+        bus.InstallEngine(); //Bus:2000cc
         Car bus1=new Bus(engine2200cc);
-        bus1.InstallEngine();
+        bus1.InstallEngine(); //Bus:2200cc
     }
 }
