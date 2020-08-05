04d123e455f88f3d89bf486b1605106699572268
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:21:52 +0800

    单例模式

--- src/Singleton/test.java
+++ src/Singleton/test.java
@@ -5,14 +5,14 @@ public class test {
     public static void main(String[] args) {
         eHanShi EHanShi = eHanShi.getEHanShi();
         eHanShi EHanShi1 = eHanShi.getEHanShi();
-        System.out.println(EHanShi==EHanShi1);
+        System.out.println(EHanShi==EHanShi1); //true
 
         lanHanShi Lan=lanHanShi.getLanHanShi();
         lanHanShi Lan1 = lanHanShi.getLanHanShi();
-        System.out.println(Lan==Lan1);
+        System.out.println(Lan==Lan1); //true
 
         shuangChongSuo shung=shuangChongSuo.getShuangChongSuo();
         shuangChongSuo shunag1=shuangChongSuo.getShuangChongSuo();
-        System.out.println(shunag1==shung);
+        System.out.println(shunag1==shung); //true
     }
 }
