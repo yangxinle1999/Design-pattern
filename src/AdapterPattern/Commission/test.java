52ac3b698343c8a59ac13d3484e86b51d49810a6
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:10:31 +0800

    使用委让方式实现的适配器模式

--- src/AdapterPattern/Commission/test.java
+++ src/AdapterPattern/Commission/test.java
@@ -3,6 +3,6 @@ package AdapterPattern.Commission;
 public class test {
     public static void main(String[] args) {
         Adapte adapte=new Adapte(new use220V());
-        adapte.use18V();
+        adapte.use18V(); //220V的电压！ 使用适配器将220V电压改造成18V
     }
 }
