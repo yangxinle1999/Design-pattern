1850c794986671b0f2f814fb25ed71dcd809df0e
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:19:25 +0800

    工厂方法模式

--- src/FactoryMethod/test.java
+++ src/FactoryMethod/test.java
@@ -4,10 +4,10 @@ public class test {
     public static void main(String[] args) {
         fruitFactory fruitFactory=new appleFactory();
         apple fruit = (apple) fruitFactory.getFruit();
-        fruit.eat();
+        fruit.eat(); //吃苹果
         fruitFactory pearFactory=new pearFactory();
         pear p = (pear) pearFactory.getFruit();
-        p.eat();
+        p.eat(); //吃梨子
         System.out.println("成功！");
     }
 }
