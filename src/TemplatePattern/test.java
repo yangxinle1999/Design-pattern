a78baaf7af630bc3cf7bad41d205aa6eead5f694
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:24:53 +0800

    模板方法模式

--- src/TemplatePattern/test.java
+++ src/TemplatePattern/test.java
@@ -5,8 +5,14 @@ public class test {
     public static void main(String[] args) {
         MakeCar makeCar=new MakeBus();
         makeCar.make();
+//        car:车头
+//        car:车身
+//        car:车尾
         System.out.println("----------");
         MakeCar makeCar1=new MakeJeep();
         makeCar1.make();
+//        jeep:车头
+//        jeep:车身
+//        jeep:车尾
     }
 }
