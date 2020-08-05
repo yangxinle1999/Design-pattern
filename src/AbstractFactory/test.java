20baecabe8705327da2604198377ae9f98b5b4e1
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:14:20 +0800

    抽象工厂模式

--- src/AbstractFactory/test.java
+++ src/AbstractFactory/test.java
@@ -4,13 +4,13 @@ public class test {
 
     public static void main(String[] args) {
         fruitFactory fruitFactory=new northFruitFactory();
-        fruitFactory.getApple().eat();
-        fruitFactory.getPera().eat();
+        fruitFactory.getApple().eat(); //北方苹果！
+        fruitFactory.getPera().eat(); //北方梨子！
 
 
         fruitFactory fruitFactory1=new southFruitFactory();
-        fruitFactory1.getPera().eat();
-        fruitFactory1.getApple().eat();
+        fruitFactory1.getPera().eat(); //南方梨子！
+        fruitFactory1.getApple().eat(); //南方苹果！
         System.out.println("成功！");
     }
 }
