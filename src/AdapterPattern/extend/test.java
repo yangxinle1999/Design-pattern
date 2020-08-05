8a4811ab40c7b2cdc63ffdf3f735dc64b517aa48
yangxinle1999 <2842611637@qq.com>
2020-08-05 14:11:11 +0800

    使用继承实现的适配器模式

--- src/AdapterPattern/extend/test.java
+++ src/AdapterPattern/extend/test.java
@@ -4,6 +4,6 @@ public class test {
 
     public static void main(String[] args) {
         Adapte adapte=new Adapte();
-        adapte.use18V();
+        adapte.use18V(); //220V的电压！ 使用适配器将220V电压改造成18V
     }
 }
